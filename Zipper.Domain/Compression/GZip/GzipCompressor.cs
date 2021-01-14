89984eb05298a275b50bf789e27533637af99101
Egor Mukhachev <stafilo13@gmail.com>
2021-01-14 19:49:53 +0400

    Первая версия архиваторов на gzip

---
+++ Zipper.Domain/Compression/GZip/GzipCompressor.cs
@@ -0,0 +1,31 @@
+﻿using System.IO;
+using System.IO.Compression;
+
+namespace Zipper.Domain.Compression.GZip
+{
+    public class GzipCompressor : ICompressor
+    {
+        private readonly int _bufferSize;
+
+        public byte[] Compress(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                return data;
+
+            using (var output = new MemoryStream())
+            {
+                using (var buf = new BufferedStream(new GZipStream(output, CompressionMode.Compress), _bufferSize))
+                {
+                    buf.Write(data, 0, data.Length);
+                }
+
+                return output.ToArray();
+            }
+        }
+
+        public GzipCompressor(int bufferSize)
+        {
+            _bufferSize = bufferSize;
+        }
+    }
+}
---
+++ Zipper.Domain/Compression/GZip/GzipDecompressor.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace Zipper.Domain.Compression.GZip
+{
+    public class GzipDecompressor : IDecompressor
+    {
+        private readonly int _bufferSize;
+
+        public byte[] Decompress(byte[] data)
+        {
+            if (data == null || data.Length == 0)
+                return data;
+
+            var buf = new byte[_bufferSize];
+            using (var output = new MemoryStream())
+            {
+                using (var input = new MemoryStream(data))
+                {
+                    using (var gzs = new BufferedStream(new GZipStream(input, CompressionMode.Decompress), buf.Length))
+                    {
+                        int count;
+                        while ((count = gzs.Read(buf, 0, buf.Length)) != 0)
+                        {
+                            Array.Resize(ref buf, count);
+                            output.Write(buf, 0, count);
+                        }
+                    }
+                }
+
+                return output.ToArray();
+            }
+        }
+
+        public GzipDecompressor(int bufferSize)
+        {
+            _bufferSize = bufferSize;
+        }
+    }
+}
