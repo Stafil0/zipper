6170af6ab557757256a3d19b5dffe52040f496d4
Egor Mukhachev <stafilo13@gmail.com>
2021-01-15 21:46:42 +0400

    Добавлен более-менее нормальный CLI

--- Zipper.CLI/Program.cs
+++ Zipper.CLI/Program.cs
@@ -1,99 +1,122 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
 using System.IO;
-using System.IO.Compression;
 using System.Linq;
+using CommandLine;
 using Zipper.Domain.BoundedBuffer.Blobs;
 using Zipper.Domain.BoundedBuffer.File;
 using Zipper.Domain.Compression.GZip;
+using Zipper.Domain.Extensions;
+using Zipper.Domain.Pipeline;
 using Zipper.Domain.Pipeline.Compression;
 
 namespace Zipper.CLI
 {
     internal class Program
     {
-        private static void GzipCompress(string input, string output)
+        private static int Compress(CompressOptions options)
         {
-            using (var inputStream = new FileStream(input, FileMode.Open))
-            using (var outputStream = new FileStream(output, FileMode.Create))
-            using (var compressionStream = new GZipStream(outputStream, CompressionMode.Compress))
+            try
             {
-                inputStream.CopyTo(compressionStream);
+                using (var inputStream = new FileStream(options.InputFile, FileMode.Open))
+                using (var outputStream = new FileStream(options.OutputFile, FileMode.Create))
+                using (var compressor = new CompressionPipeline(options.ThreadsCount, options.MaxBlobs))
+                {
+                    if (options.Verbose)
+                    {
+                        compressor.OnRead += (sender, args) => PrintReadProgress(inputStream.Length, args);
+                        compressor.OnWrite += (sender, args) => PrintCompressionLevel(inputStream.Length, args);
+                    }
+
+                    compressor
+                        .Reader(new FileStreamReader(options.BufferSize))
+                        .Writer(new BlobWriter())
+                        .Compress(inputStream, outputStream, new GzipCompressor());
+
+                    compressor.ThrowIfException();
+                }
             }
+            catch (AggregateException e)
+            {
+                var messages = string.Join(";", e.InnerExceptions.Select(x => x.Message));
+                Console.WriteLine($"One or more errors occurred while compressing file \"{options.InputFile}\" to \"{options.OutputFile}\": {messages}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error occured while compressing file \"{options.InputFile}\" to \"{options.OutputFile}\": {e.Message}");
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static int Decompress(DecompressOptions options)
+        {
+            try
+            {
+                using (var inputStream = new FileStream(options.InputFile, FileMode.Open))
+                using (var outputStream = new FileStream(options.OutputFile, FileMode.Create))
+                using (var compressor = new CompressionPipeline(options.ThreadsCount, options.MaxBlobs))
+                {
+                    if (options.Verbose)
+                    {
+                        compressor.OnRead += (sender, args) => PrintReadProgress(inputStream.Length, args);
+                        compressor.OnWrite += (sender, args) => PrintDecompressionSize(args);
+                    }
+
+                    compressor
+                        .Reader(new BlobReader())
+                        .Writer(new FileStreamWriter())
+                        .Decompress(inputStream, outputStream, new GzipDecompressor());
+                    
+                    compressor.ThrowIfException();
+                }
+            }
+            catch (AggregateException e)
+            {
+                var messages = string.Join(";", e.InnerExceptions.Select(x => x.Message));
+                Console.WriteLine($"One or more errors occurred while decompressing file \"{options.InputFile}\" to \"{options.OutputFile}\": {messages}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Error occurred while decompressing file \"{options.InputFile}\" to \"{options.OutputFile}\": {e.Message}");
+                return 1;
+            }
+
+            return 0;
+        }
+
+        private static void PrintReadProgress(long total, OnProgressEventArgs args)
+        {
+            var progress = (float) args.Progress / total * 100;
+            Console.WriteLine($"{args.Message}\tTotal read progression: {progress:0.##}%");
+        }
+
+        private static void PrintCompressionLevel(long original, OnProgressEventArgs args)
+        {
+            var compression = original / (float) args.Progress;
+            Console.WriteLine($"{args.Message}\tCompression level: {compression:0.##}%");
         }
         
-        private static void GzipDecompress(string input, string output)
+        private static void PrintDecompressionSize(OnProgressEventArgs args)
         {
-            using (var inputStream = new FileStream(input, FileMode.Open))
-            using (var outputStream = new FileStream(output, FileMode.Create))
-            using (var decompressionStream = new GZipStream(inputStream, CompressionMode.Decompress))
-            {
-                decompressionStream.CopyTo(outputStream);
-            }
+            Console.WriteLine($"Decompressed file size:\t{SizeExtensions.GetReadableSize(args.Progress)}");
         }
 
-        private static void Compress(string input, string output)
+        public static int Main(string[] args)
         {
-            using (var inputStream = new FileStream(input, FileMode.Open))
-            using (var outputStream = new FileStream(output, FileMode.Create))
-            using (var compressor = new CompressionPipeline(16, 16))
-            {
-                compressor
-                    .Reader(new FileStreamReader(1024 * 1024))
-                    .Writer(new BlobWriter())
-                    .Compress(inputStream, outputStream, new GzipCompressor());
-            }
-        }
+            return Parser.Default
+                .ParseArguments<CompressOptions, DecompressOptions>(args)
+                .MapResult(
+                    (CompressOptions options) => Compress(options),
+                    (DecompressOptions options) => Decompress(options),
+                    errors =>
+                    {
+                        Console.WriteLine("Errors while parsing options:");
+                        foreach (var error in errors)
+                            Console.WriteLine(error);
 
-        private static void Decompress(string input, string output)
-        {
-            using (var inputStream = new FileStream(input, FileMode.Open))
-            using (var outputStream = new FileStream(output, FileMode.Create))
-            using (var compressor = new CompressionPipeline(16, 16))
-            {
-                compressor
-                    .Reader(new BlobReader())
-                    .Writer(new FileStreamWriter())
-                    .Decompress(inputStream, outputStream, new GzipDecompressor());
-            }
-        }
-        
-        public static void Main(string[] args)
-        {
-            var sw = new Stopwatch();
-
-            // var completions = new List<long>();
-            // for (var i = 0; i < 100; i++)
-            // {
-            //     sw.Restart();
-            //     Compress("input.file", "compressed.file");
-            //     sw.Stop();
-            //
-            //     completions.Add(sw.ElapsedMilliseconds);
-            // }
-            // Console.WriteLine($"Compress avg = {completions.Sum() / completions.Count} milliseconds");
-            //
-            // completions = new List<long>();
-            // for (var i = 0; i < 100; i++)
-            // {
-            //     sw.Restart();
-            //     Decompress("compressed.file", "decompressed.file");
-            //     sw.Stop();
-            //
-            //     completions.Add(sw.ElapsedMilliseconds);
-            // }
-            // Console.WriteLine($"Decompress avg = {completions.Sum() / completions.Count} milliseconds");
-            
-            sw.Restart();
-            Compress("input.file", "compressed.file");
-            sw.Stop();
-            Console.WriteLine($"Compressed by {sw.ElapsedMilliseconds} milliseconds");
-            
-            sw.Restart();
-            Decompress("compressed.file", "decompressed.file");
-            sw.Stop();
-            Console.WriteLine($"Decompressed by {sw.ElapsedMilliseconds} milliseconds");
+                        return 1;
+                    });
         }
     }
 }
