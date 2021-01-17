dece878d00c064429a5e211211441397b2c68bcd
Egor Mukhachev <stafilo13@gmail.com>
2021-01-17 17:43:06 +0400

    SizeExtensionsTests

--- Zipper.Domain/Extensions/SizeExtensions.cs
+++ Zipper.Domain/Extensions/SizeExtensions.cs
@@ -4,10 +4,13 @@ namespace Zipper.Domain.Extensions
 {
     public static class SizeExtensions
     {
-        private static readonly string[] Sizes = {"B", "KB", "MB", "GB", "TB"}; 
+        private static readonly string[] Sizes = {"B", "KB", "MB", "GB"}; 
 
         public static string GetReadableSize(long i)
         {
+            if (i < 0)
+                throw new ArgumentException("Size can't be less, than 0.");
+            
             var len = Math.Abs(i);
             var order = 0;
             while (len >= 1024 && order < Sizes.Length - 1)
